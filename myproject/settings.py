5b6e66c15732098f0e9820a42965012047d13234
root <root@ip-172-31-84-75.ec2.internal>
2019-08-11 06:47:05 +0000

    update settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -39,8 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
     'posts.apps.PostsConfig',
+    'django_jenkins',
 ]
-
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+)
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
